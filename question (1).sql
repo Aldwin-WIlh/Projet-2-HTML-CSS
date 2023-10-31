6ed526fa05407cbfd60ad5b136398e0c1876bfc8
Suzaku <147034157+Aldwin-WIlh@users.noreply.github.com>
2023-10-31 09:34:54 +0100

    Update question (1).sql

--- question (1).sql
+++ question (1).sql
@@ -10,15 +10,26 @@ CREATE TABLE bonnerep (
 ); 
 
 CREATE TABLE badrep (
+  id INTEGER PRIMARY KEY NOT NULL,
   idquest INTEGER NOT NULL,
   rep TEXT NOT NULL,
   FOREIGN KEY(idquest) REFERENCES question(id)
 ); 
 
-CREATE TABLE feedback (
+CREATE TABLE goodfeedback (
   id INTEGER PRIMARY KEY NOT NULL,
+  idrep INTEGER NOT NULL
   feedback TEXT NOT NULL
+  FOREIGN KEY(idrep) REFERENCES bonnerep(id)
 ); 
+
+CREATE TABLE badfeedback (
+  id INTEGER PRIMARY KEY NOT NULL,
+  idrep INTEGER NOT NULL
+  feedback TEXT NOT NULL
+  FOREIGN KEY(idrep) REFERENCES badrep(id)
+); 
+
 
 INSERT INTO question (id, enoncer) VALUES
 (1, 'Qui est le créateur du jeu ?'),
@@ -34,59 +45,59 @@ INSERT INTO question (id, enoncer) VALUES
 (11, 'Quel personnage ci-dessous vient d’un jeu de course ?'),
 (12, 'Quel personnage utilise le célèbre ‘PK Fire’ ?');
 
-INSERT INTO bonnerep (idquest, rep) VALUES
-(1, 'Masahiro Sakurai'),
-(2, '1999'),
-(3, 'Pikachu'),
-(4, 'Waluigi'),
-(5, '82'),
-(6, 'EVO'),
-(7, 'Donkey Kong'),
-(8, 'Mewtwo'),
-(9, 'King K Rool'),
-(10, 'Zelda et Sheik'),
-(11, 'Captain Falcon'),
-(12, 'Ness');
+INSERT INTO bonnerep (id,idquest, rep) VALUES
+(1,1, 'Masahiro Sakurai'),
+(2,2, '1999'),
+(3,3, 'Pikachu'),
+(4,4, 'Waluigi'),
+(5,5, '82'),
+(6,6, 'EVO'),
+(7,7, 'Donkey Kong'),
+(8,8, 'Mewtwo'),
+(9,9, 'King K Rool'),
+(10,10, 'Zelda et Sheik'),
+(11,11, 'Captain Falcon'),
+(12,12, 'Ness');
 
-INSERT INTO badrep (idquest, rep) VALUES
-(1, 'Marc Merril'),
-(1, 'Shigeru Miyamoto'),
-(1, 'Yu Suzuki'),
-(2, '2008'),
-(2, '2001'),
-(2, '1996'),
-(3, 'sonic'),
-(3, 'bowser'),
-(3, 'zelda'),
-(4, 'snake'),
-(4, 'marth'),
-(4, 'cloud'),
-(5, '61'),
-(5, '53'),
-(5, '56'),
-(6, 'le Pound'),
-(6, 'genesis'),
-(6, 'smash summit'),
-(7, 'luigi'),
-(7, 'roi dadidou'),
-(7, 'peach'),
-(8, 'pikachu'),
-(8, 'kyurem'),
-(8, 'amphinobi'),
-(9, 'falco'),
-(9, 'corin'),
-(9, 'ganondorf'),
-(10, 'peach et daisy'),
-(10, 'link et link enfant'),
-(10, 'mario et luigi'),
-(11, 'mario'),
-(11, 'samus'),
-(11, 'chrom'),
-(12, 'lucas'),
-(12, 'bowser'),
-(12, 'felinferno');
+INSERT INTO badrep (id,idquest, rep) VALUES
+(1,1, 'Marc Merril'),
+(2,1, 'Shigeru Miyamoto'),
+(3,1, 'Yu Suzuki'),
+(4,2, '2008'),
+(5,2, '2001'),
+(6,2, '1996'),
+(7,3, 'sonic'),
+(8,3, 'bowser'),
+(9,3, 'zelda'),
+(10,4, 'snake'),
+(11,4, 'marth'),
+(12,4, 'cloud'),
+(13,5, '61'),
+(14,5, '53'),
+(15,5, '56'),
+(16,6, 'le Pound'),
+(17,6, 'genesis'),
+(18,6, 'smash summit'),
+(19,7, 'luigi'),
+(20,7, 'roi dadidou'),
+(21,7, 'peach'),
+(22,8, 'pikachu'),
+(23,8, 'kyurem'),
+(24,8, 'amphinobi'),
+(25,9, 'falco'),
+(26,9, 'corin'),
+(27,9, 'ganondorf'),
+(28,10, 'peach et daisy'),
+(29,10, 'link et link enfant'),
+(30,10, 'mario et luigi'),
+(31,11, 'mario'),
+(32,11, 'samus'),
+(33,11, 'chrom'),
+(34,12, 'lucas'),
+(35,12, 'bowser'),
+(36,12, 'felinferno');
 
-INSERT INTO feedback (id, feedback) VALUES
+INSERT INTO goodfeedback (id, feedback) VALUES
 (1,  'le créateur de super smash bros est Masahiro Sakurai'),
 (2,  'le premier jeux super smash bros a ete créé en 1999'),
 (3,  'le personnage present dans le premier opus est Pikachu'),
@@ -99,4 +110,7 @@ INSERT INTO feedback (id, feedback) VALUES
 (10,  'c est seulement depuis le precedent opus que zelda et sheik sont deux personnage distinct'),
 (11,  "c est du jeux peu connue F-zero que viens captain falcon"),
 (12,  'l attaque pk fire qui a fait rager de nombreuses personnes sur le online est une attaque de ness');
+
+
+
 
