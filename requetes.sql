4bc48953cb6b33da68b4d2c2ed88712a89fbe3b3
Maxiltron <147034131+Maxiltron@users.noreply.github.com>
2023-11-02 12:30:10 +0100

    Add files via upload

---
+++ requetes.sql
@@ -0,0 +1,20 @@
+  <!-- Liste des questions pour les utilisateurs -->
+    SELECT enoncer FROM question
+
+  <!-- Supprimer une question -->
+    DELETE  FROM 
+
+  <!-- Modifier une question -->
+    UPDATE question SET 
+
+
+  <!-- Récupérer un utilisateur -->
+    SELECT 
+
+
+  <!-- Récupérer les scores des utilisateurs -->
+    SELECT 
+
+
+  <!-- Ajouter une question -->
+
